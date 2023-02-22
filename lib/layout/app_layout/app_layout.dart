b23a28934b3cbca48ea1392adb54c79461bcb210
Ebrahim Mohamed <ebrahimmohassan15@gmail.com>
2023-02-22 19:03:55 +0200

    Added login-screen front-end code

--- lib/layout/app_layout/app_layout.dart
+++ lib/layout/app_layout/app_layout.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:graduation_project/modules/login/login_screen.dart';
 
 class AppLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(
-          'App Layout',
-        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
+      body: LoginScreen(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:graduation_project/layout/app_layout/app_layout.dart';
 
 void main() {
